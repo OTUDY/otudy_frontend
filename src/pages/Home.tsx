97fb5f5bc17fcdf580e90368a9cdaa378080b77e
Panoj-k <Panoj.ka@gmail.com>
2023-09-14 12:33:49 +0700

    add home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Simple Home Page</h1>
+      <p>
+        This is a basic example of a React home page in your application. You
+        can customize this page with your own content and components.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
