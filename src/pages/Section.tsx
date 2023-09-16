fa93c983328fbd36088074c04c6cd1906e4140a5
Panoj-k <Panoj.ka@gmail.com>
2023-09-16 15:02:47 +0700

    add class page

--- src/pages/Section.tsx
+++ src/pages/Section.tsx
@@ -1,23 +1,22 @@
 import React from "react";
 import HeaderBar from "../components/HeaderBar";
-import "../styles/HeaderBar.css";
-import "../styles/Section.css"; // Import your custom CSS file for the Section component
+import ClassTable from "../components/ClassTable";
 
 const Section = () => {
   return (
-    <div>
+    <div className="page-container">
       <div className="header-bar">
         <HeaderBar />
       </div>
 
       <div className="content">
-        <div className="left-menu"></div>
         <div className="section-content">
-          <h1>Section</h1>
+          <h1>Class</h1>
           <p>
             This is a basic example of a React Section page in your application.
             You can customize this page with your own content and components.
           </p>
+          <ClassTable />
         </div>
       </div>
     </div>
