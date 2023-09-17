f9932fcfefac2d87a3c738bda8bbe1fb5347648f
Panoj-k <Panoj.ka@gmail.com>
2023-09-17 15:21:22 +0700

    add landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home.tsx";
 import SignIn from "./pages/SignIn.tsx";
 import SignUp from "./pages/SignUp.tsx";
 import Section from "./pages/Section.tsx";
+import Landing from "./pages/Landing.tsx";
 
 const About = () => <h1>About Page</h1>;
 const Contact = () => <h1>Contact Page</h1>;
@@ -12,7 +13,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Landing />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
@@ -24,4 +25,3 @@ function App() {
 }
 
 export default App;
-
