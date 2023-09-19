cf74c90bedbe9e5f3402636fb1a70afe174cf450
Panoj-k <Panoj.ka@gmail.com>
2023-09-19 23:06:26 +0700

    add redeem button

--- src/components/RewardCard.tsx
+++ src/components/RewardCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Card, CardContent, Typography } from "@mui/material";
+import { Button, Card, CardContent, Typography } from "@mui/material";
 import RewardForm from "./RewardForm";
 
 interface RewardCardProps {
@@ -24,25 +24,36 @@ const RewardCard: React.FC<RewardCardProps> = ({
   };
 
   const handleCloseRewardForm = () => {
+    console.log("close");
     setIsRewardFormOpen(false);
   };
+
   return (
-    <div>
-      <Card
-        variant="outlined"
-        sx={{ marginBottom: 2 }}
-        onClick={handleCardClick}
+    <Card
+      variant="outlined"
+      sx={{ marginBottom: 2, position: "relative" }}
+      onClick={handleCardClick}
+    >
+      <CardContent>
+        <Typography variant="h6">{title}</Typography>
+        <Typography variant="subtitle1">Point: {point}</Typography>
+        <Typography variant="body2">Description: {description}</Typography>
+        <Typography variant="body2">Amount: {amount}</Typography>
+        <Typography variant="body2">Expired Date: {expiredDate}</Typography>
+      </CardContent>
+      <Button
+        variant="contained"
+        color="primary"
+        sx={{
+          position: "absolute",
+          bottom: 10,
+          right: 10,
+        }}
       >
-        <CardContent>
-          <Typography variant="h6">{title}</Typography>
-          <Typography variant="subtitle1">Point: {point}</Typography>
-          <Typography variant="body2">Description: {description}</Typography>
-          <Typography variant="body2">Amount: {amount}</Typography>
-          <Typography variant="body2">Expired Date: {expiredDate}</Typography>
-        </CardContent>
-      </Card>
+        Redeem
+      </Button>
       <RewardForm open={isRewardFormOpen} onClose={handleCloseRewardForm} />
-    </div>
+    </Card>
   );
 };
 
