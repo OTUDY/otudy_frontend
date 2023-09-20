b6ae4be97705bda3e07f33a652f137676db266f0
DhoungSALENE____ <phatthadon@outlook.com>
2023-09-20 23:39:57 +0700

    update: reward

--- src/components/RewardForm.tsx
+++ src/components/RewardForm.tsx
@@ -7,28 +7,49 @@ import {
   Button,
   TextField,
 } from "@mui/material";
+import axios from "axios";
 //import axios from "axios";
 //import { useNavigate } from "react-router-dom";
 
 interface RewardFormProps {
   open: boolean;
   onClose: () => void;
+  classId: string
 }
 
-const RewardForm: React.FC<RewardFormProps> = ({ open, onClose }) => {
+const RewardForm: React.FC<RewardFormProps> = ({ open, onClose, classId }) => {
   const [title, setTitle] = useState("");
-  const [point, setPoint] = useState("");
+  const [point, setPoint] = useState(0);
   const [description, setDescription] = useState("");
-  const [amount, setAmount] = useState("");
+  const [amount, setAmount] = useState(0);
   const [expiredDate, setExpiredDate] = useState("");
   //const navigate = useNavigate();
 
-  const handleCreate = () => {
+  const handleCreate = async() => {
     console.log("Title:", title);
     console.log("Point:", point);
     console.log("Description:", description);
     console.log("Amount:", amount);
     console.log("ExpiredDate:", expiredDate);
+    const body = {
+      reward_name: title,
+      reward_desc: description,
+      reward_pic: '',
+      reward_spent_points: point,
+      reward_active_status: true,
+      class_id: classId,
+      amount: amount
+    };
+    const url = `https://backend.otudy.co/api/v1/reward/create_reward`;
+    const response = await axios.post(url, body, {
+      headers: {
+        "Content-Type": 'application/json',
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      }
+    })
+    if (response.status == 200 || response.status == 201 || response.status == 202) {
+      window.location.reload();
+    }
   };
 
   return (
@@ -46,7 +67,7 @@ const RewardForm: React.FC<RewardFormProps> = ({ open, onClose }) => {
           <TextField
             label="Point to Redeem"
             value={point}
-            onChange={(e) => setPoint(e.target.value)}
+            onChange={(e) => setPoint(Number(e.target.value))}
             fullWidth
             sx={{ marginBottom: 2 }}
           />
@@ -60,7 +81,7 @@ const RewardForm: React.FC<RewardFormProps> = ({ open, onClose }) => {
           <TextField
             label="Amount"
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            onChange={(e) => setAmount(Number(e.target.value))}
             fullWidth
             sx={{ marginBottom: 2 }}
           />
