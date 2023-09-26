f56d75bd09f4bd457a833fed37b68763d479c50b
Panoj Kamolrattanawech <panoj.ka@gmail.com>
2023-09-26 10:54:11 +0700

    add reward redeem page

---
+++ src/pages/RewardRedeem.tsx
@@ -0,0 +1,112 @@
+import { useState } from "react";
+import {
+  DataGrid,
+  GridCellParams,
+  GridRowSelectionModel,
+} from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import HeaderBar from "../components/HeaderBar";
+import Typography from "@mui/material/Typography";
+
+const columns = [
+  { field: "studentId", headerName: "Student ID", width: 150 },
+  { field: "name", headerName: "Name", width: 150 },
+  { field: "lastName", headerName: "Last Name", width: 150 },
+  { field: "point", headerName: "Point", width: 150 },
+  {
+    field: "redeemed",
+    headerName: "Redeemed",
+    width: 150,
+    renderCell: (params: GridCellParams) => {
+      return params.value ? "Redeemed" : "Not Redeemed";
+    },
+  },
+];
+
+const RewardRedeem = () => {
+  const [selectionModel, setSelectionModel] = useState<GridRowSelectionModel>(
+    []
+  );
+
+  //Temp data
+  const students = [
+    {
+      student_id: "123456",
+      firstname: "John",
+      surname: "Doe",
+      point: 100,
+      redeemed: false,
+    },
+  ];
+  //   const [students, setStudents] = useState([
+  //     {
+  //       student_id: "",
+  //       firstname: "",
+  //       surname: "",
+  //       point: 0,
+  //       redeemed: false,
+  //     },
+  //   ]);
+
+  //TODO: get students from backend
+
+  const handleRedeem = () => {
+    // Update completion status for selected students
+    console.log("Redeem", selectionModel);
+  };
+
+  const handleCancle = () => {
+    // Update completion status for selected students
+    console.log("Cancle", selectionModel);
+  };
+
+  return (
+    <div className="page-container">
+      <div className="header-bar">
+        <HeaderBar />
+      </div>
+      <div className="classroom-container">
+        <div className="classroom-content">
+          <Typography variant="h4" sx={{ marginTop: "20px" }}>
+            Redeem Reward
+          </Typography>
+          <DataGrid
+            rows={students}
+            columns={columns}
+            checkboxSelection
+            rowSelectionModel={selectionModel}
+            onRowSelectionModelChange={(newSelectionModel) => {
+              setSelectionModel(newSelectionModel);
+            }}
+          />
+          <div
+            style={{
+              margin: "20px",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleCancle}
+              disabled={selectionModel.length === 0}
+            >
+              Deny
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={handleRedeem}
+              disabled={selectionModel.length === 0}
+            >
+              Complete
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RewardRedeem;
