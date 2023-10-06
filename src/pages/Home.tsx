d8c0926e99ded5e1ede16bf3e4efa86ee81c8141
DhoungSALENE____ <phatthadon@outlook.com>
2023-10-07 03:49:17 +0700

    update: home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,8 +4,7 @@ const Home = () => {
     <div>
       <h1>Welcome to My Simple Home Page</h1>
       <p>
-        This is a basic example of a React home page in your application. You
-        can customize this page with your own content and components.
+        โฮมเพจ
       </p>
     </div>
   );
